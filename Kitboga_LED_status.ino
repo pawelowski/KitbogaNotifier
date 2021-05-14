29efd780efcc7b7beadacbee512dc5f2c1d1bf52
Pav <pawel.rafalowsky@gmail.com>
2021-05-14 18:11:55 +0100

    loads of stuff

---
+++ Kitboga_LED_status.ino
@@ -0,0 +1,288 @@
+/*
+
+  3D-printed sign that shows if the Kitboga streamer is online on Twitch
+
+  Credit for STL design goes to makkuro https://www.thingiverse.com/thing:749887
+
+  Other rescourses used:
+  https://randomnerdtutorials.com/esp32-http-get-post-arduino/
+
+  /*
+   Error Codes:
+   # 1 red led = Unauthorized/Expired keys
+   # 2 red leds = Auth Issue
+   # 3 red leds = Internal Server Error
+   # ALL red = other unknown request failure
+
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+#include "FastLED.h"
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+// LED setup variables
+#define DATA_PIN 2
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS 60    //total number of used LEDs
+#define INDICAT_LED 18 // the dot over letter i
+#define BRIGHTNESS 120
+
+//Colour definitions
+CHSV yellow = CHSV(40, 240, 120);
+CHSV boga_c = CHSV(190, 239, 155);
+CHSV red = CHSV(255, 255, 130);
+CHSV orange = CHSV(23, 233, 120);
+CHSV green = CHSV(72, 213, 100);
+// =[ WiFi variables ]=
+const char *ssid = "";
+const char *password = "";
+
+// =[ Twich Helix API variables ]=
+//URLs
+const char *validateOAuthURL = "https://id.twitch.tv/oauth2/validate";
+const char *searchStreamerURL = "https://api.twitch.tv/helix/streams?user_login=kitboga";
+
+int streamersID = 32787655; //kitboga stream ID
+
+//Tokens
+String clientSecret = "";
+String clientID = "";
+String authURL = "https://id.twitch.tv/oauth2/token?client_id=" + clientID + "&client_secret=" + clientSecret + "&grant_type=client_credentials";
+String access_token = "";
+
+// =[ Other variables ]=
+unsigned long lastTime = 0;
+unsigned long timerDelay = 15; //seconds
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  Serial.begin(115200);
+  FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear(true);
+
+  poweredOn();
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  //Make the 1st requests, don't wait for the interval of "timerDelay" variable
+  validate();
+  searchStream();
+}
+
+void loop()
+{
+
+  //Send an HTTP POST request every 'timerDelay' value
+  if ((millis() - lastTime) > timerDelay * 1000)
+  {
+    //Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED)
+    {
+
+      // if validation successful, go and search the stream up
+      if (validate())
+        searchStream();
+      // otherwise, keep displaying the indication state based on the http request result
+
+      //auth(); //for debugging: check your auth separtely
+    }
+    else
+    {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+//=[ Functions ]=
+bool validate()
+{
+  //Checks if access token is valid before making a request
+  Serial.print("Validating... ");
+  //Serial.printf("Access Token: %s\n", access_token);
+  String rawResponse = httpGETRequest(validateOAuthURL, "Authorization", access_token, "", "");
+  int validTime = parseJson(rawResponse)["expires_in"];
+  bool valid;
+  if (validTime > 0)
+  {
+    valid = true;
+  }
+  else
+    valid = false;
+  Serial.printf("Keys valid for: %d sec \n", validTime);
+  delay(500);
+  return valid;
+}
+
+void auth()
+{
+  //Request a new access token to make a call to the API's endpoints
+  Serial.print("Authing... ");
+  HTTPClient http;
+  http.begin(authURL);
+
+  int httpResponseCode = http.POST("");
+
+  String payload = "{}";
+
+  // ALL GOOD
+  if (httpResponseCode == 200)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+
+    JSONVar twitchAuthResponse = parseJson(payload);
+
+    //extract access token from the Twich API response
+    access_token = JSON.stringify(twitchAuthResponse["access_token"]);
+    //Serial.println(access_token); // for debugging
+
+    //I couldn't figure out how to extract characters from inside of " ", so I just chop them off both ends.
+    // Example: access_token = "a1b2c3d4e5f6g7h8i9j1k2l3m4n5o6";
+
+    access_token.remove(0, 1); //remove 1st " (character 0)
+    //Serial.printf("Access token lenght: %d\n", access_token.length());
+    access_token.remove(access_token.length() - 1, 1); //remove last " (character lenght - 1)
+    //Serial.printf("Access Token: %s\n", access_token); // for debugging
+  }
+  else
+  {
+    Serial.print("Auth Issue");
+    errorState(1, orange);
+  }
+  http.end();
+}
+
+void searchStream()
+{
+  //Look up streamers based on their user_login (can do multiple at a time). See 'searchStreamerURL' variable
+  Serial.print("Looking up stream... ");
+  JSONVar twitchStreamResponse = JSON.parse(httpGETRequest(searchStreamerURL, "Authorization", access_token, "Client-Id", clientID));
+  if (JSON.typeof(twitchStreamResponse) == "undefined")
+  {
+    Serial.println("Parsing input failed!");
+  }
+
+  //int resultLength = twitchStreamResponse["data"].length();
+
+  if ((int(twitchStreamResponse["data"].length()) > 0) && (atoi(twitchStreamResponse["data"][0]["user_id"]) == streamersID) && (JSON.stringify(twitchStreamResponse["data"][0]["type"]) == (const char *)("\"live\"")))
+  {
+    //if ( (resultLength > 0) && (atoi(twitchStreamResponse["data"][0]["user_id"]) == streamersID ) && (JSON.stringify(twitchStreamResponse["data"][0]["type"]) == (const char*)("\"live\"") )) {
+    Serial.println("=^= KIT IS LIVE BITCHES !!! GATHER AROUND! =^=");
+    kitOnline();
+  }
+  else
+  {
+    Serial.println("KIT IS OFFILINE =[");
+    kitOffline();
+  }
+}
+
+JSONVar parseJson(String _stringResult)
+{
+  JSONVar twitchResponse = JSON.parse(_stringResult);
+  if (JSON.typeof(twitchResponse) == "undefined")
+  {
+    Serial.println("Parsing input failed!");
+    //    return;
+  }
+  return twitchResponse;
+}
+
+String httpGETRequest(const char *reqPath, String _auth_h, String _auth_v, String _c_id_h, String _c_id_v)
+{
+
+  HTTPClient http;
+  http.begin(reqPath);
+
+  //http.addHeader("Content-Type", "text/plain");
+  http.addHeader(_c_id_h, _c_id_v);
+  String auth_param = "Bearer " + _auth_v;
+  http.addHeader(_auth_h, auth_param, false, false);
+
+  int httpResponseCode = http.GET();
+
+  String payload = "{}";
+
+  // ALL GOOD.
+  if (httpResponseCode == 200)
+  {
+    Serial.printf("HTTP Response code: %d\n", httpResponseCode);
+    payload = http.getString();
+
+    // Unauthorized - expired keys, so re-auth.
+  }
+  else if (httpResponseCode == 401)
+  {
+    errorState(1, yellow); //yellow
+    Serial.printf("Error code: %d\n", httpResponseCode);
+    Serial.println("Unauthorized.");
+    auth();
+
+    //Internal Server Error.
+  }
+  else if (httpResponseCode == 500)
+  {
+    errorState(1, red);
+    Serial.printf("Error code: %d\n", httpResponseCode);
+    Serial.println("Internal Server Error");
+    timerDelay = 15 * 60; // try in 15min
+    Serial.printf("Will try next in %d sec\n", timerDelay);
+  }
+  else
+  {
+    Serial.println("Unexpected... !");
+    Serial.printf("Error code: %d\n", httpResponseCode);
+    errorState(NUM_LEDS, red);
+  }
+
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+//=[ LED indication states ]=
+void kitOnline()
+{
+  fill_solid(leds, NUM_LEDS, boga_c);
+  FastLED.show();
+  delay(100);
+}
+
+void kitOffline()
+{
+  FastLED.clear(true);
+  leds[INDICAT_LED] = boga_c;
+  FastLED.show();
+}
+
+void errorState(int num, CHSV colour)
+{
+  FastLED.clear(true);
+  fill_solid(leds, num, colour);
+  FastLED.show();
+}
+void poweredOn()
+{
+  FastLED.clear(true);
+  leds[INDICAT_LED] = green;
+  FastLED.show();
+}
--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# kitbota-status
-3D-printed sign that shows if the Kitboga streamer is online on Twitch
+\> Work In Progress <
+
+Still documenting the repo
+
+# Kitboga Twitch Status
+
+3D-printed sign that shows if the Kitboga streamer is online on Twitch.
+
+### TO DO:
+
+[ ] save `access_token` to EEPROM memory
+
+[ ] fix error codes
+
+## Description
+
+Project uses an ESP32 to call the TwitchTV Helix API to check status of Kitboga's stream. The result is displayed on a 3D-printed sign with addressible RGBs and FastLED library.
+
+NOTE: The STLs are a small remix of a great design by makkuro [thing:749887](https://www.thingiverse.com/thing:749887).
+
+# Details
+
+When my fav streamer Kitboga (https://www.twitch.tv/kitboga) comes up live I get notified with all the LEDs lighting up in the stream's colour.
+
+![Kit-online](https://github.com/pawelowski/KitbogaTwitchStatus/blob/main/photos/KIT_BOGA_online-part.jpg)
+
+When he's offline, only 1 LED over _i_ illuminates.
+
+![Kit-offline](https://github.com/pawelowski/KitbogaTwitchStatus/blob/main/photos/KIT_offline.jpg)
+
+Also error states are displayed.
+
+### Error Codes
+
+- :yellow_circle: = Unauthorized/Expired keys
+- :orange_circle: = Auth Issue
+- :red_circle: = Internal Server Error
+- :cursing_face: ALL red = other unknown request failure (including response `-1`)
+
+## Hardware BoM
+
+- ESP32
+- WS2811 addressible RGBs
+- 3D-printed letters (printed separately)
+- resistor, capacitor, diode, power connector, wires
+
+## Libriaries and other necessary resources
+
+- access to Twich Helix API https://dev.twitch.tv/docs/api/
+- HTTPClient https://www.arduino.cc/reference/en/libraries/httpclient/
+- Arduino JSON https://www.arduino.cc/reference/en/libraries/arduino_json/
+- FastLED https://www.arduino.cc/reference/en/libraries/fastled/
+
+## ESP32 setup
+
+![circuit_diagram](https://github.com/pawelowski/KitbogaTwitchStatus/blob/main/photos/circuit_diagram.JPG)
+
+- diode D1 provents power being drawn from the ESP32
+- capacitor C1 fixes [Timed out waiting for packet header](https://randomnerdtutorials.com/solved-failed-to-connect-to-esp32-timed-out-waiting-for-packet-header/)
+
+## API setup
+
+1. created a Twitch developer account to obtain access keys https://dev.twitch.tv/docs/api/
+
+2. in Arduino IDE, filled `clientID` & `clientSecret` accordingly. Line #156 to prints auth token in Serial Monitor.
+   Uploaded code to the the μC.
+
+NOTE: Also used programes clients like Postman or Insomnia to preview the token myself.
+
+3. Auth token was generated from the API and printed in the Serial Monitor. Grabed it and pasted to `access_token` in Arduino IDE (it lasts for around 60 days, this prevents creating a new one when I often power cycle the device in early stages). Commented back the #156 line, re-uploaded the code with a valid `auth_token`.
+
+Erros are shown in the Serial Monitor for debugging. Further lines from the code can be uncommented to print important variables.
+
+## STL and printing details
+
+![KiT_STL](https://raw.githubusercontent.com/pawelowski/KitbogaTwitchStatus/main/photos/KiT_SLT_screenshot.JPG?v4&s=1260px)
+![boga_STL](https://raw.githubusercontent.com/pawelowski/KitbogaTwitchStatus/main/photos/boga_STL_screenshot.JPG)
+
+- nothing special here, regular 0.2mm nozzle and low infil 15%
+
+# Know issue
+
+The Twich API requires HTTPS requests. However, I only managed to make this work with HTTP via HTTPClient. This, once in a while, causes the ESP32 to fail to call the API - generates http response code `-1`. A power cycle or reboot of the ESP seems to mostly fix it.. this is horrible and annoying so if you want to help and develop proper HTTPS calls (maybe with [WiFiClientSecure](https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFiClientSecure) lib) then plz get involved <3
+
+# More photos
+
+![](https://github.com/pawelowski/KitbogaTwitchStatus/blob/main/photos/STL_LED_inside.jpg)
